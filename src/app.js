1fdd35d542d46925e182f91d342cdaf6f56892da
Gabriel Rabelo <31012788+gabrielrab@users.noreply.github.com>
2019-07-10 15:42:07 -0300

    10-07

--- src/app.js
+++ src/app.js
@@ -2,11 +2,17 @@ const express = require('express');
 const ejs = require('ejs');
 const multer = require('multer');
 
+
 const app = express();
 
+app.set('views', __dirname +'/views');
+app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res)=>{ res.render('index'); })
+app.get('/', (req, res)=>{ res.render('index'); });
+app.post('/upload', (req, res)=>{
+
+});
 
 app.listen(3000 || process.env.PORT, ()=>{
     console.log('Api rodando...')
