1b2b45c010db8fcb306728d603a7ea8b4084527e
Gabriel Rabelo <31012788+gabrielrab@users.noreply.github.com>
2019-07-11 11:43:33 -0300

    Code Working

--- src/app.js
+++ src/app.js
@@ -1,17 +1,29 @@
 const express = require('express');
 const ejs = require('ejs');
-const multer = require('multer');
-
 
 const app = express();
-
 app.set('views', __dirname +'/views');
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res)=>{ res.render('index'); });
-app.post('/upload', (req, res)=>{
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: (req, file, cb)=>{
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb)=>{
+        cb(null, file.fieldname+'-'+Date.now()+".png");
+    }
+});
 
+const upload = multer({storage: storage});
+
+app.get('/', (req, res)=>{ res.render('index'); });
+
+app.post('/', upload.single('img'), (req, res)=>{
+    console.log(req.body, req.file, req.files);
+
+    return res.send('Ok');
 });
 
 app.listen(3000 || process.env.PORT, ()=>{
