cfb46b82857b3f57db39909afc9cc55cf1ce41e1
Gabriel Rabelo <31012788+gabrielrab@users.noreply.github.com>
2019-07-11 15:40:34 -0300

    descarte

--- src/app.js
+++ src/app.js
@@ -1,25 +1,24 @@
 const express = require('express');
 const ejs = require('ejs');
-
 const app = express();
-app.set('views', __dirname +'/views');
-app.use(express.static(__dirname + '/public'));
-app.set('view engine', 'ejs');
 
+//Multer
 const multer = require('multer');
 const storage = multer.diskStorage({
     destination: (req, file, cb)=>{
         cb(null, 'uploads/');
     },
     filename: (req, file, cb)=>{
-        cb(null, file.fieldname+'-'+Date.now()+".png");
+        cb(null, Date.now()+'-'+file.originalname);
     }
 });
-
 const upload = multer({storage: storage});
 
-app.get('/', (req, res)=>{ res.render('index'); });
+//Comprimir imagens
+const fileHelper = require('./service/file-helper');
 
+//Rotas
+app.get('/', (req, res)=>{ res.render('index'); });
 app.post('/', upload.single('img'), (req, res)=>{
     console.log(req.body, req.file, req.files);
 
@@ -29,3 +28,4 @@ app.post('/', upload.single('img'), (req, res)=>{
 app.listen(3000 || process.env.PORT, ()=>{
     console.log('Api rodando...')
 });
+
